6ea220cd613407246816f0c26590e32949dbaa1b
rgusarev <gusarer@gmail.com>
2025-12-05 00:12:32 +0100

    feat: irregular verbs (essere, avere, fare, andare, stare)

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ const vocabularies = {
   "italian-common-nouns": { name: "Italian common words", url: "https://raw.githubusercontent.com/rgusarev/igen_irtem/refs/heads/main/italian_common_nouns.csv" },
   "italian-numerals": { name: "Italian numerals", url: "https://raw.githubusercontent.com/rgusarev/igen_irtem/refs/heads/main/italian_numerals.csv" },
   "italian-irr-verbs": { name: "Italian irregular verbs", url: "https://raw.githubusercontent.com/rgusarev/igen_irtem/refs/heads/main/italian_irregular_verbs_conj.csv" },
+  "italian-irr-conj": { name: "Italian irregular conjugation", url: "https://raw.githubusercontent.com/rgusarev/igen_irtem/refs/heads/main/italian_irr_conj.csv" },
   "italian-regular-are": { name: "Italian regular -are verbs", url: "https://raw.githubusercontent.com/rgusarev/igen_irtem/refs/heads/main/italian_regular_are.csv" },
   "italian-introductory": { name: "Italian introductory words", url: "https://raw.githubusercontent.com/rgusarev/igen_irtem/refs/heads/main/italian_introductory_words.csv" },
   "IT-RU-food": { name: "It-RU raw food", url: "https://raw.githubusercontent.com/rgusarev/igen_irtem/refs/heads/main/it_ru_food.csv" },
